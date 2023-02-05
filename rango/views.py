072037466d3aa02ea9808623da405dd20f789785
Chase Testwuide <2819186t@student.gla.ac.uk>
2023-02-05 17:05:00 +0000

    passed chapter 8 tests

--- rango/views.py
+++ rango/views.py
@@ -37,32 +37,21 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
-    
 
 def add_category(request):
     form = CategoryForm()
-    
-    # A HTTP POST?
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
-            # The supplied form contained errors -
-            
-            # just print them to the terminal.
             print(form.errors)
-
-    # Will handle the bad form, new form, or no form supplied cases.
-    # Render the form with error messages (if any).
-    return render(request, 'rango/add_category.html', {'form': form})
     
+    return render(request, 'rango/add_category.html', {'form': form})
+
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -71,7 +60,7 @@ def add_page(request, category_name_slug):
     
     # You cannot add a page to a Category that does not exist... DM
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
