ad07f12f55503092c9a500e4ba26ed18a461330c
Chase Testwuide <2819186t@student.gla.ac.uk>
2023-02-21 19:56:42 +0000

    finished chapter 12!

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -137,3 +137,16 @@ LOGIN_URL = 'rango:login'
 
 #cookie length
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
